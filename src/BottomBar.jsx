fb04fc7bfaba1babf0d727f9880bf82aebae5099
leemch <lee7mchale@gmail.com>
2020-03-16 00:06:45 -0700

    added best buy style to search bar and store locator

--- src/BottomBar.jsx
+++ src/BottomBar.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const BottomBar = props => (
     <div className="bottombar">
+        <div className="searchbarcontainer">
             <input placeholder="Search Best Buy" />
-            <span className="bottombar btns">
-                <button type="button">Culver City WestField</button>
-                <button type="button">Cart</button>
-            </span>    
+            <button className="searchbutton">Search</button>
+        </div>
+
+        <div className="buttonscontainer">
+            <button type="button" className="btn-unstyled"><span className="store-display-name">Culver City WestField</span></button>
+            <button type="button">Cart</button>
+        </div>
     </div>
 
 )
